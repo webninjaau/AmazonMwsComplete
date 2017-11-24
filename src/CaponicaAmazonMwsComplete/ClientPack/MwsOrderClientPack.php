2c26454fdd1654030cd9c96d3aa53ef5f7b2b967
Brett Bailey <bretto36@hotmail.com>
2017-11-24 15:01:42 +1000

    Added List Order Items Calls

--- src/CaponicaAmazonMwsComplete/ClientPack/MwsOrderClientPack.php
+++ src/CaponicaAmazonMwsComplete/ClientPack/MwsOrderClientPack.php
@@ -8,28 +8,31 @@ use CaponicaAmazonMwsComplete\Domain\Throttle\ThrottleAwareClientPackInterface;
 use CaponicaAmazonMwsComplete\Domain\Throttle\ThrottledRequestManager;
 
 class MwsOrderClientPack extends MwsOrderClient implements ThrottleAwareClientPackInterface {
-    const PARAM_AMAZON_ORDER_IDS            = 'AmazonOrderId';
-    const PARAM_CREATED_AFTER               = 'CreatedAfter';
-    const PARAM_CREATED_BEFORE              = 'CreatedBefore';
-    const PARAM_LAST_UPDATED_AFTER          = 'LastUpdatedAfter';
-    const PARAM_MARKETPLACE_ID              = 'MarketplaceId';
-    const PARAM_MARKETPLACE_ID_LIST         = 'MarketplaceId.Id.1';
-    const PARAM_MERCHANT                    = 'SellerId';
-    const PARAM_NEXT_TOKEN                  = 'NextToken';
-    const PARAM_ORDER_STATUS_LIST           = 'OrderStatus';
+    const PARAM_AMAZON_ORDER_IDS                = 'AmazonOrderId';
+    const PARAM_AMAZON_ORDER_ID                 = 'AmazonOrderId';
+    const PARAM_CREATED_AFTER                   = 'CreatedAfter';
+    const PARAM_CREATED_BEFORE                  = 'CreatedBefore';
+    const PARAM_LAST_UPDATED_AFTER              = 'LastUpdatedAfter';
+    const PARAM_MARKETPLACE_ID                  = 'MarketplaceId';
+    const PARAM_MARKETPLACE_ID_LIST             = 'MarketplaceId.Id.1';
+    const PARAM_MERCHANT                        = 'SellerId';
+    const PARAM_NEXT_TOKEN                      = 'NextToken';
+    const PARAM_ORDER_STATUS_LIST               = 'OrderStatus';
 
-    const STATUS_PENDING_AVAILABILITY       = 'PendingAvailability';
-    const STATUS_PENDING                    = 'Pending';
-    const STATUS_UNSHIPPED                  = 'Unshipped';
-    const STATUS_PARTIALLY_SHIPPED          = 'PartiallyShipped';
-    const STATUS_SHIPPED                    = 'Shipped';
-    const STATUS_INVOICE_UNCONFIRMED        = 'InvoiceUnconfirmed';
-    const STATUS_CANCELED                   = 'Canceled';
-    const STATUS_UNFULFILLABLE              = 'Unfulfillable';
+    const STATUS_PENDING_AVAILABILITY           = 'PendingAvailability';
+    const STATUS_PENDING                        = 'Pending';
+    const STATUS_UNSHIPPED                      = 'Unshipped';
+    const STATUS_PARTIALLY_SHIPPED              = 'PartiallyShipped';
+    const STATUS_SHIPPED                        = 'Shipped';
+    const STATUS_INVOICE_UNCONFIRMED            = 'InvoiceUnconfirmed';
+    const STATUS_CANCELED                       = 'Canceled';
+    const STATUS_UNFULFILLABLE                  = 'Unfulfillable';
 
-    const METHOD_GET_ORDER                  = 'getOrder';
-    const METHOD_LIST_ORDERS                = 'listOrders';
-    const METHOD_LIST_ORDERS_BY_NEXT_TOKEN  = 'listOrdersByNextToken';
+    const METHOD_GET_ORDER                      = 'getOrder';
+    const METHOD_LIST_ORDERS                    = 'listOrders';
+    const METHOD_LIST_ORDERS_BY_NEXT_TOKEN      = 'listOrdersByNextToken';
+    const METHOD_LIST_ORDER_ITEMS               = 'listOrdersItems';
+    const METHOD_LIST_ORDER_ITEMS_BY_NEXT_TOKEN = 'listOrdersItemsByNextToken';
 
     /** @var string $marketplaceId      The MWS MarketplaceID string used in API connections */
     protected $marketplaceId;
@@ -108,6 +111,24 @@ class MwsOrderClientPack extends MwsOrderClient implements ThrottleAwareClientPa
         return CaponicaClientPack::throttledCall($this, self::METHOD_LIST_ORDERS_BY_NEXT_TOKEN, $requestArray);
     }
 
+    public function callListOrderItems($id) {
+        $requestArray = [
+            self::PARAM_MERCHANT        => $this->sellerId,
+            self::PARAM_AMAZON_ORDER_ID => $id,
+        ];
+
+        return CaponicaClientPack::throttledCall($this, self::METHOD_LIST_ORDER_ITEMS, $requestArray);
+    }
+
+    public function callListOrderItemsByNextToken($nextToken) {
+        $requestArray = [
+            self::PARAM_MERCHANT   => $this->sellerId,
+            self::PARAM_NEXT_TOKEN => $nextToken,
+        ];
+
+        return CaponicaClientPack::throttledCall($this, self::METHOD_LIST_ORDER_ITEMS, $requestArray);
+    }
+
     // ###################################################
     // # ThrottleAwareClientPackInterface implementation #
     // ###################################################
@@ -119,6 +140,8 @@ class MwsOrderClientPack extends MwsOrderClient implements ThrottleAwareClientPa
                 self::METHOD_GET_ORDER                  => [6, 0.015],
                 self::METHOD_LIST_ORDERS                => [6, 0.015],
                 self::METHOD_LIST_ORDERS_BY_NEXT_TOKEN  => [null, null, null, self::METHOD_LIST_ORDERS],
+                self::METHOD_LIST_ORDERS                => [30, 0.5], // 30 per second, 1 every 2 seconds
+                self::METHOD_LIST_ORDERS_BY_NEXT_TOKEN  => [null, null, null, self::METHOD_LIST_ORDERS_BY_NEXT_TOKEN ],
             ]
         );
     }
